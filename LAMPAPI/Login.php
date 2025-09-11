c4d2b2fca4217b4e88d427df5d2e7771f3d35c3e
Kevin Rodriguez <129116757+KevinTTO5@users.noreply.github.com>
2025-09-10 21:00:42 -0400

    Update Login.php to work with code.js and front end

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -35,7 +35,7 @@ if ($row = $result->fetch_assoc()) {
         ]
     ]);
 } else {
-    http_response_code(401); // Unauthorized
+    http_response_code(401);
     echo json_encode(["success" => false, "error" => "Invalid credentials"]);
 }
 
